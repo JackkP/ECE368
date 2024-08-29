d3b0cca8e4c80fbcdae0d770215a3b520761685a
Jonathan Kyle Porter <porte208@eceprog3.ecn.purdue.edu>
2024-08-29 19:19:01 -0400

    hopefully this submission doesn't kill the autograder

--- A1/main.c
+++ A1/main.c
@@ -5,20 +5,12 @@
  */
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 int main(int argc, char ** argv)
 {
-    /*if(argc < 2) {
-        printf("Too few arguments supplied\n");
-        return 1;
-    }*/
-
     // read in the change to make
     int val;
     scanf("%d", &val);
-    //val = atoi(argv[1]);
     // count down from the maximum number of possible each coin (except pennies) and print
     for (int q=val/25;q>=0;q--)
         for(int d=(val-q*25)/10;d>=0;d--)
