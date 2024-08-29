8d254d602cec9f074312f9e619cc7b4e2c9035c1
Jonathan Kyle Porter <porte208@ececomp2.ecn.purdue.edu>
2024-08-29 14:42:43 -0400

    code is functional

--- A1/main.c
+++ A1/main.c
@@ -10,6 +10,13 @@
 
 int main(int argc, char ** argv)
 {
-	
-	
+    // read in the change to make
+    int val;
+    scanf("%d", &val);
+    // count down from the maximum number of possible each coin (except pennies) and print
+    for (int q=val/25;q>=0;q--)
+        for(int d=(val-q*25)/10;d>=0;d--)
+            for(int n=(val-q*25-d*10)/5;n>=0;n--)
+                printf("%d quarter(s), %d dime(s), %d nickel(s), %d pennie(s)\n", q, d, n, val-q*25-d*10-n*5);
+
 }
