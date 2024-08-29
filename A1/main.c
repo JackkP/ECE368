942423635e0716f3924a0ecff8960d5e7f14d55d
Jonathan Kyle Porter <porte208@ececomp2.ecn.purdue.edu>
2024-08-29 15:40:26 -0400

    changed input and zipped

--- A1/main.c
+++ A1/main.c
@@ -10,13 +10,21 @@
 
 int main(int argc, char ** argv)
 {
+    if(argc < 2) {
+        printf("Too few arguments supplied\n");
+        return 1;
+    }
+
     // read in the change to make
     int val;
-    scanf("%d", &val);
+    //scanf("%d", &val); //artifact from my misinterpretation of "standard input" (see A1_updated.pdf)
+    val = atoi(argv[1]);
     // count down from the maximum number of possible each coin (except pennies) and print
     for (int q=val/25;q>=0;q--)
         for(int d=(val-q*25)/10;d>=0;d--)
             for(int n=(val-q*25-d*10)/5;n>=0;n--)
                 printf("%d quarter(s), %d dime(s), %d nickel(s), %d pennie(s)\n", q, d, n, val-q*25-d*10-n*5);
+
+    return 0;
 
 }
